cb103aa6f36550e16ed53f971d2f290ff360b79b
Richard Song <brsong22@gmail.com>
2016-01-08 01:09:59 -0600

    added titles to navigation bar and changed font style for title

--- SnapDecision/SnapDecision/ViewController.swift
+++ SnapDecision/SnapDecision/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.navigationController!.navigationBar.titleTextAttributes = [ NSFontAttributeName: UIFont(name: "Noteworthy-Bold", size: 20)!]
     }
 
     override func didReceiveMemoryWarning() {
