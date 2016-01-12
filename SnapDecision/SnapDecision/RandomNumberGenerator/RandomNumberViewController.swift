21220c45e3f8ef7d6391501cbdce55f6bb26a7ae
Richard Song <brsong22@gmail.com>
2016-01-11 20:37:01 -0600

    added new class and view for random number generation - need to have ui components adjust to view size

---
+++ SnapDecision/SnapDecision/RandomNumberGenerator/RandomNumberViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RandomNumberViewController.swift
+//  SnapDecision
+//
+//  Created by Richard Song on 1/11/16.
+//  Copyright © 2016 brsong22. All rights reserved.
+//
+
+import UIKit
+
+class RandomNumberViewController: UIViewController {
+
+    @IBOutlet weak var rangeTextFloor: UITextField!
+    @IBOutlet weak var rangeTextCeiling: UITextField!
+}
