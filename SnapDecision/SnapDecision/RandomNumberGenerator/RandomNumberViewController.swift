7557f10e414b15c66edfe11ed858545361006e14
Richard Song <brsong22@gmail.com>
2016-01-13 20:55:26 -0600

    changed random # generator layout; working on updating stepper value <-> text field value

--- SnapDecision/SnapDecision/RandomNumberGenerator/RandomNumberViewController.swift
+++ SnapDecision/SnapDecision/RandomNumberGenerator/RandomNumberViewController.swift
@@ -8,8 +8,45 @@
 
 import UIKit
 
-class RandomNumberViewController: UIViewController {
+class RandomNumberViewController: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet weak var rangeTextFloor: UITextField!
-    @IBOutlet weak var rangeTextCeiling: UITextField!
+    @IBOutlet weak var lowerLimitText: UITextField!
+    @IBOutlet weak var lowerLimitStepper: UIStepper!
+    
+    @IBOutlet weak var upperLimitText: UITextField!
+    @IBOutlet weak var upperLimitStepper: UIStepper!
+    
+    @IBOutlet weak var quantityText: UITextField!
+    @IBOutlet weak var quantityStepper: UIStepper!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.lowerLimitText.delegate = self
+        self.upperLimitText.delegate = self
+        self.quantityText.delegate = self
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissNumpad")
+        view.addGestureRecognizer(tap)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func lowerLimitAdjust(sender: UIStepper) {
+        lowerLimitText.text = Int(sender.value).description
+    }
+    
+    func dismissNumpad(){
+        view.endEditing(true)
+    }
+    
+    func updateStepValue(textFieldChanged: String){
+        if textFieldChanged == "lowerLimit" {
+            lowerLimitStepper.value = Double(Int(lowerLimitText.text!)!)
+        }
+    }
+    
 }
